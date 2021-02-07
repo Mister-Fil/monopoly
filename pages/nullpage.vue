ca06c33c6a25ca3b4fb5fc124d0b825931345efd
dead1ike <faithcreed@mail.ru>
2021-02-08 02:55:47 +0500

    update package

--- pages/nullpage.vue
+++ pages/nullpage.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="container pt-3 d-flex flex-column text-center">
-    <div>
-      <b-alert show>Бросок Кубиков</b-alert>
-    </div>
-    <div>Dice 1: {{ dice1 }}</div>
-    <div>Dice 2: {{ dice2 }}</div>
-    <div></div>
-    <div>Sum {{ diceSumm }}</div>
-    <div class="text-center">
-      <b-button pill variant="outline-danger" @click="roll()"
-        >Lets Roll!</b-button
+  <div class="d-flex justify-content-center vh-100 vw-100 text-center">
+    <div class="align-self-center">
+      <h3
+        v-anime="{
+          rotate: -360,
+          backgroundColor: ['#ea021a', '#ffffff'],
+          duration: 4500,
+          loop: false,
+        }"
+        style="width: 250px"
       >
+        Бросок кубиков
+      </h3>
+      <div></div>
+      <div>Dice 1: {{ dice1 }}</div>
+      <div>Dice 2: {{ dice2 }}</div>
+      <div></div>
+      <div>Sum {{ diceSumm }}</div>
+      <div class="text-center">
+        <b-button pill variant="outline-danger" @click="roll()"
+          >Lets Roll!</b-button
+        >
+      </div>
     </div>
   </div>
 </template>
