f23fca9c98392c57ddc68f4cac10d39af0c861f5
user <dearocosmocat@gmail.com>
2021-03-12 11:03:43 +0200

    changed game-field add click to b-btn add script

--- pages/lesson.vue
+++ pages/lesson.vue
@@ -1,55 +1,29 @@
 <template>
   <div>
     <div>
-      <b-btn class="btn-danger" to="/">Give up</b-btn>
+      <b-btn class="btn-danger" href="/">Quit</b-btn>
     </div>
     <div class="game_field d-flex align-content-stretch flex-wrap">
-      <game-field ref="a1" class="game_cell"></game-field>
-      <div ref="a2" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">a2</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="a3" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">a3</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="b1" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">b1</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="b2" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">b2</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="b3" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">b3</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="c1" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">c1</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="c2" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">c2</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
-      <div ref="c3" class="game_cell">
-        <h3 class="text-capitalize font-weight-light">c3</h3>
-        <div class="heart text-center py"><b-icon icon="heart-fill" font-scale="4"></b-icon></div>
-      </div>
+      <game-field
+        v-for="item in fields"
+        :key="item.column + item.row"
+        :column="item.column"
+        :row="item.row"
+        :heart="field.heart"
+      ></game-field>
     </div>
-    <div class="heart text-center py-3">
+    <div v-if="field.heart === ''" class="heart text-center py-3">
       <b-icon icon="heart-fill" font-scale="4"></b-icon>
     </div>
     <h3 class="text-center font-italic">Steps</h3>
     <div>
       <div class="text-center py-2">
-        <b-btn class="btn-dark"><b-icon icon="arrow-up"></b-icon></b-btn>
+        <b-btn @click="up" class="btn-dark"><b-icon icon="arrow-up"></b-icon></b-btn>
       </div>
       <div class="text-center">
-        <b-btn class="btn-dark"><b-icon icon="arrow-left"></b-icon></b-btn>
-        <b-btn class="btn-dark mx-1"><b-icon icon="arrow-down"></b-icon></b-btn>
-        <b-btn class="btn-dark"><b-icon icon="arrow-right"></b-icon></b-btn>
+        <b-btn @click="left" class="btn-dark"><b-icon icon="arrow-left"></b-icon></b-btn>
+        <b-btn @click="down" class="btn-dark mx-1"><b-icon icon="arrow-down"></b-icon></b-btn>
+        <b-btn @click="right" class="btn-dark"><b-icon icon="arrow-right"></b-icon></b-btn>
       </div>
     </div>
   </div>
@@ -67,5 +41,48 @@
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      field: {
+        columns: 3,
+        rows: 3,
+        heart: '',
+      },
+      columnMap: [{ 1: 'a' }, { 2: 'b' }, { 3: 'c' }, { 4: 'd' }, { 5: 'e' }],
+    }
+  },
+  computed: {
+    fields() {
+      const fields = []
+      let rows = 1
+      while (rows <= this.field.rows) {
+        let columns = 1
+        while (columns <= this.field.columns) {
+          const element = this.columnMap.find((element) => element[columns])
+          fields.push({
+            row: rows,
+            column: element[columns],
+          })
+          columns++
+        }
+        rows++
+      }
+      return fields
+    },
+  },
+  methods: {
+    up() {
+      this.field.heart = 'b3'
+    },
+    down() {
+      this.field.heart = ''
+    },
+    left() {
+      this.field.heart = 'a3'
+    },
+    right() {
+      this.field.heart = 'c3'
+    },
+  },
 }
 </script>
